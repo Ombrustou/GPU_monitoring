1694da0d650b382a98a19b1902095ce725e23866
Ombrustou <costamagnalou@gmail.com>
2023-04-25 19:34:58 +0200

    adding mongo part

---
+++ Gathering_DATA/querying.py
@@ -0,0 +1,50 @@
+import paramiko
+import time
+from pymongo import MongoClient
+
+mongoClient = MongoClient('localhost', 27017)
+db = mongoClient.GPU_monitoring
+
+computer_notResponding = {}
+
+while True:
+    with open('query_targets.txt', 'r') as f:
+        content = f.read()
+        #Parse different
+        computers = content.split("\n")
+        #Parse each line
+        computers = [computer.split(",") for computer in computers]
+
+        for computer in computers:
+            client = paramiko.SSHClient()
+            client.load_system_host_keys()
+            try:
+                client.connect(computer[0], username=computer[1], password=computer[2])
+
+
+                stdin, stdout, stderr = client.exec_command('nvidia-smi --query-gpu=index,name,temperature.gpu,memory.total,memory.used --format=csv index, name, temperature.gpu, memory.total [MiB], memory.used [MiB]')
+                output = stdout.read().decode()
+                
+                gpus = output.split("\n")[1:-1]
+                gpus = [gpu.split(", ") for gpu in gpus]
+
+                obj = []
+                for gpu in gpus:
+                    obj.append({"number":int(gpu[0]),"name":gpu[1],"temperature":float(gpu[2]),"max_capacity":float(gpu[3].split(" ")[0]),"usage":float(gpu[4].split(" ")[0])})
+
+                print(obj)    
+                #db.data.update_one({"IP":computer[0]},{$set {"IP":computer[0],"GPU":obj, "lost_connect":False}},{ "upsert" : "true" })
+            except:
+                if(computer[0] in computer_notResponding):
+                    computer_notResponding[computer[0]] += 1
+                    """
+                    if(computer_notResponding[computer[0]] >= 3):
+                        db.data.update_one({"IP":computer[0]},{$set {"IP":computer[0], "lost_connect":True, "GPU":[]}},{ "upsert" : "true" })
+                    """
+                else:
+                    computer_notResponding[computer[0]] = 1 
+            finally:
+                client.close()
+        
+    
+    time.sleep(1)
