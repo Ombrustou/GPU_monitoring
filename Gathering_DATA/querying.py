0483910a363d2c2a0f478d511892b1789b0ddef7
Ombrustou <costamagnalou@gmail.com>
2023-04-26 12:42:47 +0200

    Adding commentary

--- Gathering_DATA/querying.py
+++ Gathering_DATA/querying.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 mongoClient = MongoClient('localhost', 27017)
 db = mongoClient.GPU_monitoring
 
+#Create a dict to store number of response not
 computer_notResponding = {}
 
 while True:
@@ -16,8 +17,11 @@ while True:
         computers = [computer.split(",") for computer in computers]
 
         for computer in computers:
+            #Create a client
             client = paramiko.SSHClient()
             client.load_system_host_keys()
+
+            #If we can connect to the remote computer
             try:
                 client.connect(computer[0], username=computer[1], password=computer[2])
 
@@ -31,17 +35,21 @@ while True:
                 obj = []
                 for gpu in gpus:
                     obj.append({"number":int(gpu[0]),"name":gpu[1],"temperature":float(gpu[2]),"max_capacity":float(gpu[3].split(" ")[0]),"usage":float(gpu[4].split(" ")[0])})
+    
+                computer_notResponding[computer[0]] = 0
 
-                print(obj)    
-                #db.data.update_one({"IP":computer[0]},{$set {"IP":computer[0],"GPU":obj, "no_response":0}},{ "upsert" : "true" })
+                #Update the document for the IP, if it doesn't exist insert a new one
+                db.data.update_one({"IP":computer[0]},{"$set" : {"IP":computer[0],"GPU":obj, "no_response":0}},True)
+            #If the remote computer is unreachable
             except:
                 if(computer[0] in computer_notResponding):
                     computer_notResponding[computer[0]] += 1
-                    #db.data.update_one({"IP":computer[0]},{$set {"IP":computer[0], "no_response":computer_notResponding[computer[0]], "GPU":[]}},{ "upsert" : "true" })
+                    #Update the document for the IP, if it doesn't exist insert a new one
+                    db.data.update_one({"IP":computer[0]},{"$set": {"IP":computer[0], "no_response":computer_notResponding[computer[0]], "GPU":[]}},True)
                 else:
                     computer_notResponding[computer[0]] = 1 
             finally:
                 client.close()
         
     
-    time.sleep(1)
+    time.sleep(300)
