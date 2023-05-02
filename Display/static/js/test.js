64916f6924c12db78c6e97514d6b2602e55409cc
Titouan_J <jamet.e2101129@etud.univ-ubs.fr>
2023-05-02 16:24:33 +0200

    Main structure established

--- Display/static/js/test.js
+++ Display/static/js/test.js
@@ -1,32 +1,329 @@
+
 const app = Vue.createApp({
-    template: `
-      <div :class="['sidebar', 'sidebar-right', { collapsed: collapsed }]">
-        <div class="sidebar-header" @click="toggle">
-            <img src="resources/media/notif.jpeg">
-        </div>
-        <div class="sidebar-content">
-          <slot></slot>
-        </div>
-      </div>
-    `,
+
     data() {
       return {
-        collapsed: true
+        collapsed: true,
+        computerList:[{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },
+        {
+          IP: "192.168.123.124",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        },{
+          IP: "192.168.123.123",
+          no_response: 1,
+          GPU: [{
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+          },
+          {
+            id: 1,
+            name: "NVIDIA GeForce GTX 1080 Ti"
+
+          }]
+        }]
       }
     },
     methods: {
       toggle() {
         this.collapsed = !this.collapsed;
+      }, 
+      gather() {
+        axios.get('http://localhost:3001/monitoring')
+        .then(function (response){
+          computerList = response.data
+          console.log(response);
+        })
+        .catch(function (error){
+          //handle error
+          console.log(error);a
+          alert("Error From Api");
+        });
+      },
+      test: function() {
+        this.testVa = this.testVa + 1
+        console.log(this.testVa)
       }
-    }
-  });
-  
-  app.component('sidebar', {
-    template: `
-      <div>
-        <slot></slot>
-      </div>
-    `
+    },
+
+    mounted(){
+      console.log('App Mounted');
+      setInterval(() => {
+        this.gather();
+      }, 600000);
+  }
   });
   
   app.mount('.app');
