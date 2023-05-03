0c1571e4064ec5d3a48e35fbd7c818e2c79031cb
Ombrustou <costamagnalou@gmail.com>
2023-05-03 18:49:45 +0200

    computers id are now in the database

--- Gathering_DATA/querying.py
+++ Gathering_DATA/querying.py
@@ -10,48 +10,45 @@ db = mongoClient.monitoring
 computer_notResponding = {}
 
 while True:
-    with open('query_targets.txt', 'r') as f:
-        content = f.read()
-        #Parse different
-        computers = content.split("\n")
-        #Parse each line
-        computers = [computer.split(",") for computer in computers]
+    # Query the collection for documents with the 'name' field equal to 'John'
+    computers = db.computer.find({})
 
-        for computer in computers:
-            #Create a client
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # Iterate over the cursor and print each document
+    for computer in computers:
+        #Create a client
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-            #If we can connect to the remote computer
-            try:
-                client.connect(computer[0], username=computer[1], password=computer[2])
+        #If we can connect to the remote computer
+        try:
+            client.connect(computer['IP'], username=computer['username'], password=computer['password'])
 
+            stdin, stdout, stderr = client.exec_command('nvidia-smi --query-gpu=index,name,temperature.gpu,memory.total,memory.used --format=csv index, name, temperature.gpu, memory.total [MiB], memory.used [MiB]')
+            output = stdout.read().decode()
 
-                stdin, stdout, stderr = client.exec_command('nvidia-smi --query-gpu=index,name,temperature.gpu,memory.total,memory.used --format=csv index, name, temperature.gpu, memory.total [MiB], memory.used [MiB]')
-                output = stdout.read().decode()
+            gpus = output.split("\n")[1:-1]
+            gpus = [gpu.split(", ") for gpu in gpus]
+
+            obj = []
+            for gpu in gpus:
+                obj.append({"number":int(gpu[0]),"name":gpu[1],"temperature":float(gpu[2]),"max_capacity":float(gpu[3].split(" ")[0]),"usage":float(gpu[4].split(" ")[0])})
+
+            computer_notResponding[computer['IP']] = 0
+
+            #Update the document for the IP, if it doesn't exist insert a new one
+            db.data.update_one({"IP":computer['IP']},{"$set" : {"IP":computer['IP'],"GPU":obj, "no_response":0}},True)
+        #If the remote computer is unreachable
+        except:
+            print("Couldn't reach ", computer['IP'])
+            if(computer['IP'] in computer_notResponding):
+                computer_notResponding[computer['IP']] += 1
+            else:
+                computer_notResponding[computer['IP']] = 1 
                 
-                gpus = output.split("\n")[1:-1]
-                gpus = [gpu.split(", ") for gpu in gpus]
-
-                obj = []
-                for gpu in gpus:
-                    obj.append({"number":int(gpu[0]),"name":gpu[1],"temperature":float(gpu[2]),"max_capacity":float(gpu[3].split(" ")[0]),"usage":float(gpu[4].split(" ")[0])})
-    
-                computer_notResponding[computer[0]] = 0
-
-                #Update the document for the IP, if it doesn't exist insert a new one
-                db.data.update_one({"IP":computer[0]},{"$set" : {"IP":computer[0],"GPU":obj, "no_response":0}},True)
-            #If the remote computer is unreachable
-            except:
-                if(computer[0] in computer_notResponding):
-                    computer_notResponding[computer[0]] += 1
-                else:
-                    computer_notResponding[computer[0]] = 1 
-                    
-                #Update the document for the IP, if it doesn't exist insert a new one
-                db.data.update_one({"IP":computer[0]},{"$set": {"IP":computer[0], "no_response":computer_notResponding[computer[0]], "GPU":[]}},True)
-            finally:
-                client.close()
+            #Update the document for the IP, if it doesn't exist insert a new one
+            db.data.update_one({"IP":computer['IP']},{"$set": {"IP":computer['IP'], "no_response":computer_notResponding[computer['IP']], "GPU":[]}},True)
+        finally:
+            client.close()
         
     
     time.sleep(3)
