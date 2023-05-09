b9ab21e466a5eff262e53a733b0e37035ba3accc
YxandAr <lolo.game522@gmail.com>
2023-05-09 14:23:39 +0200

    The API can now modify a computer

--- API/server.js
+++ API/server.js
@@ -80,6 +80,22 @@ const connectToDatabase = async () => {
       }
     })
 
+    app.put('/computer/:ip/:IP/:username/:password', async (req, res) => {
+      const computerCollection = dbMongo.collection('computer');
+      try{
+        const result = await computerCollection.updateOne({IP: String(req.params.ip)},{$set: {IP: req.params.IP, username: req.params.username, password: req.params.password}});
+        console.log(result)
+        if(result.modifiedCount === 1){
+          res.sendStatus(204)
+        }else{
+          res.sendStatus(404)
+        }
+      }catch (err){
+        console.log(err);
+        res.status(500).send('Error while updating a computer')
+      }
+    })
+
     // Start the server
     const port = process.env.PORT || 3001;
     app.listen(port, () => {
